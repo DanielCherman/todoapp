67672961afd69c8e72856eac1da1f6dbdcdefc83
Daniel Cherman <32578659+DanielCherman@users.noreply.github.com>
2020-04-04 00:44:50 +0200

    added

---
+++ src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsa totam, illo quidem, at voluptates facere ipsum necessitatibus harum quibusdam id placeat nemo, cumque omnis praesentium exercitationem rem. Ullam, tempora.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, quaerat cumque? Asperiores magni at maxime numquam, magnam soluta nemo laboriosam enim tenetur. Eum nisi delectus accusantium earum in minima quae.</p>
+    
+
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
